971249b06d35bfff1cd8075878d0313e0caf6a5b
AlinaKrasnova171RMC069 <125280932+AlinaKrasnova171RMC069@users.noreply.github.com>
2023-04-12 22:15:50 +0300

    commit1

--- hash_substring.py
+++ hash_substring.py
@@ -13,20 +13,53 @@ def read_input():
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    select = input()[0]
+    if "I" in select:
+        pattern = input()
+        text = input()
+    elif "F" in select:
+        with open('./test/06', 'r') as f:
+            pattern = f.readline()
+            text = f.readline()
+    return pattern, text
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
-
+    
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
-
+    prime = 10**9+7
     # and return an iterable variable
-    return [0]
+    return RB_algorithm(pattern, text, prime)    
 
+def RB_algorithm(pattern, text, prime):
+    M = len(pattern)
+    N = len(text)
+    d = 256
+    q = prime
+    h = pow(d, M-1, q)
+    P = 0
+    T = 0
+    result = []
+    
+    for i in range(M):
+        P = (P*d + ord(pattern[i])) % q
+        T = (T*d + ord(text[i])) % q
+
+    for i in range(N-M+1):
+        if P == T:
+            if pattern == text[i:i+M]:
+                result.append(i)
+
+        if i < N-M:
+            T = (d*(T-ord(text[i])*h) + ord(text[i+M])) % q
+
+    return result
 
 # this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
 
+    
+  
